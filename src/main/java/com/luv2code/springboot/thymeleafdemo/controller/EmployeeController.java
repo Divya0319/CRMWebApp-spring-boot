e712248a00aa3762dfaed81bdb6f36011a38e46d
Divya Gupta <divygupta0319@gmail.com>
2021-06-22 15:55:20 +0530

    Added mapping /list to EmployeeController

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -40,5 +41,12 @@ public class EmployeeController {
 	}
 	
 	// add mapping for /list
-
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+		
+		// add to the spring model
+		theModel.addAttribute("employees", theEmployees);
+		
+		return "list-employees";
+	}
 }
