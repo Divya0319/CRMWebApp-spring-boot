ca23aaee160e170ad1ac9580f82401342bbb6a10
Divya Gupta <divygupta0319@gmail.com>
2021-06-22 20:41:59 +0530

    Added index.html to redirect to list of employees, added mapping to fetch list of employees and show in table

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,9 +1,6 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,38 +8,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
-
-	// load employee data
 	
-	private List<Employee> theEmployees;
+	private EmployeeService employeeService;
 	
-	@PostConstruct
-	private void loadData() {
-		
-		// create employees
-		Employee emp1 = new Employee(1, "Leslie", "Andrews", "leslie@gmail.com");
-		Employee emp2 = new Employee(1, "Marcus", "Brutus", "mark@gmail.com");
-		Employee emp3 = new Employee(1, "Franklin", "Clinton", "frank@gmail.com");
-		Employee emp4 = new Employee(1, "Trevor", "Philips", "trevorphilipsindustries@gmail.com");
-		
-		// create the list
-		theEmployees = new ArrayList<>();
-		
-		// add to the list
-		
-		theEmployees.add(emp1);
-		theEmployees.add(emp2);
-		theEmployees.add(emp3);
-		theEmployees.add(emp4);
+	public EmployeeController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
 	}
 	
 	// add mapping for /list
 	@GetMapping("/list")
 	public String listEmployees(Model theModel) {
+		
+		List<Employee> theEmployees = employeeService.findAll();
 		
 		// add to the spring model
 		theModel.addAttribute("employees", theEmployees);
