d643012afbeb4c406636f3544e1fd0d5b0f0f3e2
Divya Gupta <divygupta0319@gmail.com>
2021-06-23 17:04:28 +0530

    Created config classes for datasources and spring security configs

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/config/DemoSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.luv2code.springboot.thymeleafdemo.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	// add a reference to our security datasource
+	
+	@Autowired
+	@Qualifier("securityDataSource")
+	private DataSource securityDataSource;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		// use jdbc authentication .. oh yeah!!!
+		auth.jdbcAuthentication().dataSource(securityDataSource);
+	}
+	
+	
+
+}
